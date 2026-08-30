5f90a2d43ee1634983fce5af422b9a6df5672856
xfnansaid <mohammadafnan761@gmail.com>
2026-08-30 19:43:51 +0530

    q

--- src/components/editor/index.js
+++
@@ -1,2 +0,0 @@
-export { default as LiveEditorToolbar } from './LiveEditorToolbar';
-export { getEditorCSSVars, getTextStyleCSS, FONT_OPTIONS, COLOR_PALETTES, SECTION_PRESETS } from './LiveEditorToolbar';
